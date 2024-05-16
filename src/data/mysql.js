316df4197f9787b382b98cf654c21a24c2702e6e
alilopez <ali.lopez37@gmail.com>
2024-05-16 15:53:55 -0600

    First commit

---
+++ src/data/mysql.js
@@ -0,0 +1,25 @@
+const mysql = {
+    products : [
+        {
+            image: 'vite.svg',
+            text: 'dkjfhlkjsdfljashdklfjhasdklfhksd'
+        },{
+            image: 'vite.svg',
+            text: 'dkjfhlkjsdfljashdklfjhasdklfhksd'
+        },
+        {
+            image: 'vite.svg',
+            text: 'dkjfhlkjsdfljashdklfjhasdklfhksd'
+        },
+        {
+            image: 'vite.svg',
+            text: 'dkjfhlkjsdfljashdklfjhasdklfhksd'
+        },
+        {
+            image: 'node.png',
+            text: 'dkjfhlkjsdfljashdklfjhasdklfhksd'
+        }
+    ]
+}
+
+export default mysql;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+
+import Section from "../components/organisms/Section";
+
+function Home() {
+    return (<Section></Section>  );
+}
+
+export default Home;
